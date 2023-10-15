797271029b1461be70289cc583e1d20ddc427650
Camon-fi07 <422nikitos244@mail.ru>
2023-10-15 12:53:55 +0700

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
+      utils: "/src/utils",
+      App: "/src/App",
+      store: "/src/store",
+    },
+  },
+});
