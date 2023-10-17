e7b40cea339a7e0dae3bf7b57c351bb20ed60a1f
Camon-fi07 <422nikitos244@mail.ru>
2023-10-17 21:53:15 +0700

    Добавил типизированные хуки для redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,3 +3,7 @@ import userReducer from "./reducers/userSlice";
 const rootReducer = combineReducers({ userReducer });
 
 export const setupStore = () => configureStore({ reducer: rootReducer, devTools: true });
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
