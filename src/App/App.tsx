bbe8e9ff633f592ea222f8f9f696078ca918de20
Camon-fi07 <422nikitos244@mail.ru>
2023-10-19 18:34:38 +0700

    Удалил ненужные импорты из App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,6 @@
-import { Registration } from "pages/Registration/Registration";
 import "./styles/app.scss";
 import { Head } from "components/Head/Header";
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 const App = () => {
   return (
     <div className="app">
