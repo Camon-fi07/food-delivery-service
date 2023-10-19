48ea4cc988fcae276ec72a7b71bd3eea253053d0
Camon-fi07 <422nikitos244@mail.ru>
2023-10-19 18:17:13 +0700

    добавил routing и путь до страничек

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,53 @@
 import { Registration } from "pages/Registration/Registration";
 import "./styles/app.scss";
 import { Head } from "components/Head/Header";
-function App() {
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Menu</div>,
+    },
+    {
+      path: "/registration",
+      element: <Registration />,
+    },
+    {
+      path: "/login",
+      element: <div>Login</div>,
+    },
+    {
+      path: "/profile",
+      element: <div>Profile</div>,
+    },
+    {
+      path: "/item/:id",
+      element: <div>Item</div>,
+    },
+    {
+      path: "/cart",
+      element: <div>Cart</div>,
+    },
+    {
+      path: "/purchase",
+      element: <div>Purchase</div>,
+    },
+    {
+      path: "/orders",
+      element: <div>Orders</div>,
+    },
+    {
+      path: "/order/:id",
+      element: <div>Order</div>,
+    },
+  ]);
+
   return (
     <div className="app">
       <Head />
-      <Registration />
+      <RouterProvider router={router} />
     </div>
   );
-}
+};
 
 export default App;
