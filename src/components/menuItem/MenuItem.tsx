1267cabc6bc19fdbed6739ffe8010511e22fa617
Camon-fi07 <422nikitos244@mail.ru>
2023-10-23 19:39:22 +0700

    Сверстал элемент меню

--- src/components/menuItem/MenuItem.tsx
+++ src/components/menuItem/MenuItem.tsx
@@ -4,14 +4,16 @@ import style from "./style.module.scss";
 export const MenuItem = (props: DishDto) => {
   return (
     <article className={style.menu_item}>
-      <img src={props.image} />
+      <div className={style.image}>
+        <img src={props.image} />
+      </div>
       <h2 className={style.title}>{props.name}</h2>
       <p className={style.categories}>
         <span>Категория блюда</span> - {props.category}
       </p>
       <p className={style.rating}>{props.rating}</p>
       <p className={style.description}>{props.description}</p>
-      <p className={style.cost}>{props.price}</p>
+      <p className={style.cost}>{props.price} ₽</p>
       <div className={style.buy_menu}>
         <button>Купить</button>
       </div>
