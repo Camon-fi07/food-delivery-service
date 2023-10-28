267b2e2ce37d7e56a8628f4cb687d5143572ef03
Camon-fi07 <422nikitos244@mail.ru>
2023-10-28 23:22:24 +0700

    Сверстал элемент корзины

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -5,14 +5,18 @@ import { CartItemInfo } from "utils/types/CartItemInfo";
 export const CartItem = (props: CartItemInfo) => {
   return (
     <article className={style.cart_item}>
-      <span>{props.index}</span>
+      <span className={style.index}>{props.index}.</span>
       <div className={style.image}>
         <img src={props.dish.image} />
       </div>
-      <h2 className={style.title}>{props.dish.name}</h2>
-      <p className={style.cost}>{props.dish.price}</p>
+      <div className={style.description}>
+        <h2 className={style.title}>{props.dish.name}</h2>
+        <p className={style.cost}>Цена/шт: {props.dish.price} ₽</p>
+      </div>
       <DishCountPanel dishId={props.dish.id} />
-      <button onClick={props.delete} className={style.delete_btn}></button>
+      <button onClick={props.delete} className={style.delete_btn}>
+        Удалить
+      </button>
     </article>
   );
 };
--- src/components/dishesList/DishesList.tsx
+++ src/components/dishesList/DishesList.tsx
@@ -9,20 +9,23 @@ export const DishesList = ({ dishes }: { dishes: DishBasketDto[] }) => {
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.userReducer);
   return (
-    <ul className={style.dishes_list}>
+    <section className={style.dishes_list}>
       {dishes.map((dish, index) => (
-        <li>
-          <CartItem
-            delete={() => {
-              deleteDish(user.data.token, dish.id, () => {
+        <CartItem
+          delete={() => {
+            deleteDish(
+              user.data.token,
+              dish.id,
+              () => {
                 dispatch(getCart(user.data.token));
-              });
-            }}
-            dish={dish}
-            index={index + 1}
-          />
-        </li>
+              },
+              false,
+            );
+          }}
+          dish={dish}
+          index={index + 1}
+        />
       ))}
-    </ul>
+    </section>
   );
 };
