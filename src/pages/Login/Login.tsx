315202f235ca80049bec36ce79695eea39470e14
Camon-fi07 <422nikitos244@mail.ru>
2023-10-29 15:57:12 +0700

    Сделал уведомления для логина

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,27 +1,34 @@
 import { Authorization } from "utils/types/User";
 import { useAppDispatch, useAppSelector } from "utils/hooks/redux";
 import { getToken } from "store/reducers/user/UserAsyncActions";
-import { userLogin } from "utils/consts/apiUrls";
-import style from "./style.module.scss";
 import { useNavigate } from "react-router-dom";
 import { loginInitValues } from "utils/consts/formsInitValues";
 import { CustomForm } from "components/customForm/CustomForm";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect } from "react";
+import { userLogin } from "utils/consts/apiUrls";
+import style from "./style.module.scss";
 
 export const Login = () => {
   const dispatch = useAppDispatch();
-  const { error } = useAppSelector((state) => state.userReducer);
+  const { error, isLoading } = useAppSelector((state) => state.userReducer);
   const navigate = useNavigate();
   const onSubmit = (values: Authorization) => {
     dispatch(getToken({ path: userLogin, value: values })).then(() => {
-      navigate("/");
+      if (!error) navigate("/");
     });
   };
+
+  useEffect(() => {
+    if (error) toast.error(error, { theme: "dark", autoClose: 1500 });
+    else if (isLoading) toast.warning(isLoading, { theme: "dark", autoClose: 1500 });
+  }, [error, isLoading]);
 
   const initValues = loginInitValues(onSubmit);
 
   return (
     <div className={style.login}>
-      {error ? <span>{error}</span> : ""}
+      <ToastContainer />
       <h1 className={style.title}>Логин</h1>
       <CustomForm<Authorization> {...initValues} />
     </div>
