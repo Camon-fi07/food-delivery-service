97296e0b4e78fc03770bc2ab2646a112bdfe840e
Camon-fi07 <422nikitos244@mail.ru>
2023-10-29 13:09:29 +0700

    Добавил ссылку на страницу с покупкой - Сделал функцию для вычисления общей суммы заказа - Сделал ссылку перехода на страницу с покупкой

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,6 +1,8 @@
 import { useAppSelector } from "utils/hooks/redux";
 import style from "./style.module.scss";
 import { DishesList } from "components/dishesList/DishesList";
+import { Link } from "react-router-dom";
+import { getTotalPrice } from "utils/helpers/getTotalPrice";
 
 export const Cart = () => {
   const { dishes, error, isLoading } = useAppSelector((state) => state.cartReducer);
@@ -9,8 +11,12 @@ export const Cart = () => {
 
   return (
     <div className={style.cart}>
-      <h2>Товары в корзине</h2>
+      <h2 className={style.title}>Товары в корзине</h2>
       <DishesList dishes={dishes} />
+      <Link className={style.link} to={"/purchase"}>
+        Верно, к оплате
+        <span>{getTotalPrice(dishes)} ₽</span>
+      </Link>
     </div>
   );
 };
