cac6a61457ed2256a8cf7a89ff237b61ed34fee5
Camon-fi07 <422nikitos244@mail.ru>
2023-10-30 21:21:42 +0700

    Сделал ссылку на страницу оформление заказа - Если в корзине есть товары, то появляется соответствующая ссылка - Адаптивное отображение данного блока

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,8 +1,21 @@
+import { useAppSelector } from "utils/hooks/redux";
 import style from "./style.module.scss";
+import { Link } from "react-router-dom";
 
 export const Orders = () => {
+  const cart = useAppSelector((state) => state.cartReducer);
   return (
     <div className={style.orders}>
+      {cart.dishes.length ? (
+        <div className={style.purchase_alert}>
+          <span>В корзине есть блюда, можно оформить заказ</span>
+          <Link className={style.link} to={"/purchase"}>
+            Оформить
+          </Link>
+        </div>
+      ) : (
+        ""
+      )}
       <h2 className={style.title}>Последние заказы</h2>
     </div>
   );
