bbc8ad2ec8cb7fd3596c8216193613fdc8616203
Camon-fi07 <422nikitos244@mail.ru>
2023-10-30 21:03:58 +0700

    Страница с историей заказов - Создал страницу с историями заказов - Изменил router

---
+++ src/pages/Orders/Orders.tsx
@@ -0,0 +1,9 @@
+import style from "./style.module.scss";
+
+export const Orders = () => {
+  return (
+    <div className={style.orders}>
+      <h2 className={style.title}>Последние заказы</h2>
+    </div>
+  );
+};
