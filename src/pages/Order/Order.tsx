61c5d05e4638325df1efc8aa061026566bb7dac9
Camon-fi07 <422nikitos244@mail.ru>
2023-11-01 20:37:55 +0700

    Подтвердение доставки - Добавил подтверждение при  нажатии на кнопку

--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -3,18 +3,20 @@ import { ToastContainer, toast } from "react-toastify";
 import { specificOrder } from "utils/consts/apiUrls";
 import { useGetRequest } from "utils/hooks/useGetRequest";
 import { OrderDto, OrderStatus, OrderStatusTranslate } from "utils/types/Order";
-import style from "./style.module.scss";
 import { DishesList } from "components/dishesList/DishesList";
 import { useAppSelector } from "utils/hooks/redux";
+import style from "./style.module.scss";
+import { confirmOrder } from "utils/helpers/confirmOrder";
 
 export const Order = () => {
   const { token } = useAppSelector((state) => state.userReducer).data;
   const { id } = useParams();
-  const { data } = useGetRequest<OrderDto>(
+  const { data, getData } = useGetRequest<OrderDto>(
     specificOrder(id!),
     () => toast.error("Произошла ошибка при загрузке", { theme: "dark", autoClose: 1000 }),
     token,
   );
+
   return (
     <div className={style.order}>
       <ToastContainer />
@@ -23,7 +25,9 @@ export const Order = () => {
           <div className={style.head}>
             <h2 className={style.id}>Заказ {data.id}</h2>
             {data.status === OrderStatus.InProcess ? (
-              <button className={style.confirm}>Подтвердить доставку</button>
+              <button onClick={() => confirmOrder(id!, token, () => getData())} className={style.confirm}>
+                Подтвердить доставку
+              </button>
             ) : (
               ""
             )}
@@ -42,13 +46,13 @@ export const Order = () => {
           </p>
           <span>Список блюд</span>
           <DishesList dishes={data.dishes} canChange={false} />
-          <p>
+          <p className={style.price}>
             <b>Стоимость заказа: </b>
             {data.price} руб.
           </p>
         </>
       ) : (
-        <p className={style.price}>Loading...</p>
+        <p>Loading...</p>
       )}
     </div>
   );
