4786db151ac8ae190951e0c46a3d14e7e34c7947
Camon-fi07 <422nikitos244@mail.ru>
2023-11-05 15:58:21 +0700

    Маска для телефона - исправил алгоритм для маски - добавил алгоритм в компонент формы

--- src/utils/helpers/phoneMask.ts
+++ src/utils/helpers/phoneMask.ts
@@ -1,27 +1,24 @@
 export const phoneMask = (
-  oldValue: string,
   e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>,
+  oldValue: string,
 ) => {
-  if (e.target.value.length < oldValue.length) {
-    return e;
-  }
-  const originValue = e.target.value;
+  if (e.target.value.length < oldValue.length) return e;
+  const originValue = e.target.value.replace(/[\s()]/g, "");
   let newValue = "";
   for (let i = 0; i < originValue.length; i++) {
     if (i === 0) {
       if (originValue[i] === "8") newValue += "+7 (";
+      else if (originValue.length === 1 && originValue[i] === "+") newValue += "+";
       else if (originValue[i] === "+" && originValue[i + 1] === "7") {
         newValue += "+7 (";
-        i += 3;
+        i += 1;
       }
       continue;
     }
     newValue += originValue[i];
-    if (i === 6 && i === originValue.length - 2) {
-      newValue += ") ";
-    }
-    if (i === 11 && i === originValue.length - 2) newValue += " ";
-    if (i === 14 && i === originValue.length - 2) newValue += " ";
+    if (i === 4) newValue += ") ";
+    if (i === 7) newValue += " ";
+    if (i === 9) newValue += " ";
   }
   const temp: typeof e = Object.assign(e);
   temp.target.value = newValue;
